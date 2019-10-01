a0daa22bc6be41970db2d950755bee110c13e57c
Pavlo Pyvovar <jalovanfres@gmail.com>
2019-10-01 22:27:41 +0300

    Minor fix of a01, a1 merged with a2, a02a completed

--- scripts/assignment01a.py
+++ scripts/assignment01a.py
@@ -1,21 +1,22 @@
 chars = ['malt', 'rat', 'cat',
-         'dog', 'cow with the crumpled horn',
-         'maiden all forlorn',
-         'priest all shaven and shorn',
-         "cock that crow'd in the morn",
-         'farmer sowing his corn']
+        'dog', 'cow with the crumpled horn',
+        'maiden all forlorn',
+        'man all tattered and torn',
+        'priest all shaven and shorn',
+        "cock that crowed in the morn",
+        'farmer sowing his corn']
 
-actions = ['Jack built,', 'lay in the house that Jack built.',
-           'ate the malt,', "kill'd the rat,", 'worried the cat,',
-           "tossed the dog,", "milk'd the cow with the crumpled horn,",
-           "kissed the maiden all forlorn,",
-            "married the man all tatter'd and torn,",
-           "waked the priest all shaven and shorn,",
-           "kept the cock that crow'd in the morn,"]
+actions = ['lay in the house that Jack built.',
+            'ate the malt', "killed the rat,", 'worried the cat,',
+            "tossed the dog,", "milked the cow with the crumpled horn,",
+            "kissed the maiden all forlorn,",
+            "married the man all tattered and torn,",
+            "waked the priest all shaven and shorn,",
+            "kept the cock that crowed in the morn,"]
 
 print("This is the house that Jack built.\n")
-for i, char in enumerate(chars, 1):
+for i, char in enumerate(chars):
     print(f'This is the {char},')
-    for action in reversed(actions[1:i+1]):    
+    for action in reversed(actions[:i+1]):    
         print(f"That {action}")
     print()
--- scripts/assignment02a.py
+++ scripts/assignment02a.py
@@ -7,14 +7,43 @@ merge assignment01 and assignment02
 Wrap Assignment 1-A in function `poem()` that satisfies the doctest:
 
 >>> from pathlib import Path
->>> poem() == Path('data/poem-en.txt').read_text()
+>>> poem() == Path('../data/poem-en.txt').read_text()
 True
 """
 
 def poem():
-    return ''
+    chars = ['malt', 'rat', 'cat',
+            'dog', 'cow with the crumpled horn',
+            'maiden all forlorn',
+            'man all tattered and torn',
+            'priest all shaven and shorn',
+            "cock that crowed in the morn",
+            'farmer sowing his corn']
+
+    actions = ['lay in the house that Jack built.',
+                'ate the malt', "killed the rat,", 'worried the cat,',
+                "tossed the dog,", "milked the cow with the crumpled horn,",
+                "kissed the maiden all forlorn,",
+                "married the man all tattered and torn,",
+                "waked the priest all shaven and shorn,",
+                "kept the cock that crowed in the morn,"]
+
+    story = "This is the house that Jack built.\n"
+
+    for i, char in enumerate(chars):
+        story += '\n'
+        if char != 'malt':
+            story += f'This is the {char},' + '\n'
+        else:
+            story += f'This is the {char}' + '\n'
+        for action in reversed(actions[:i+1]):    
+            story += f"That {action}" + '\n'
+    return story
 
 
 if __name__ == '__main__':
+    # print(poem())
+    # with open('mypoem.txt', 'w') as f:
+    #     f.write(poem())
     import doctest
     doctest.testmod()
