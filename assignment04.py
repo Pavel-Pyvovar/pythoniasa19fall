23b54cf62569be668e46ab6c6ab1c018eb36f029
Pavlo Pyvovar <jalovanfres@gmail.com>
2019-11-03 12:48:52 +0200

    Assignment 4 completed

--- assignment04.py
+++ assignment04.py
@@ -6,7 +6,7 @@ def task1(text):
     >>> task1('89,9,-789, 0, 1')
     [89, 9, -789, 0, 1]
     """
-    # todo: write your code here
+    return [int(num) for num in text.split(',')]  
 
 
 def task2(text):
@@ -16,7 +16,7 @@ def task2(text):
     >>> task2('pen pineapple apple pen')
     'apple pen pen pineapple'
     """
-    # todo: write your code here
+    return ' '.join(w for w in sorted(text.split()))
 
 
 def task3(text):
@@ -29,24 +29,25 @@ def task3(text):
     >>> task3('Найди себе дело по душе и тебе не придётся трудиться ни одного дня в жизни. (Конфуций)')
     {'digits': 0, 'letters': 26}
     """
-    # todo: write your code here
+    return {'digits': len({c for c in text if c.isdigit()}),
+            'letters': len({c for c in text if c.isalpha()})}
 
 
-def task4(digit):
+def task4(digit, n):
     """
     For any digit X from 0 to 9, calculate expression 'X + XX + XXX + XXXX'.
 
-    >>> [task4(d) for d in '0123456789']
+    >>> [task4(d, 4) for d in '0123456789']
     [0, 1234, 2468, 3702, 4936, 6170, 7404, 8638, 9872, 11106]
     """
-    # todo: write your code here
+    return sum(int(digit * i) for i in range(1, n+1))
 
 
 def task5(text, letter1, letter2):
     """
     You are given three inputs: a string, one letter, and a second letter.
     Write a function that returns True if every instance of the first letter
-        occurs before every instance of the second letter.
+    occurs before every instance of the second letter.
 
     >>> task5('a rabbit jumps joyfully', 'a', 'j')
     True
@@ -59,12 +60,17 @@ def task5(text, letter1, letter2):
     >>> task5('happy birthday', 'z', 'a')
     False
     """
-    # todo: write your code here
+    let1_pos = [i for i, let in enumerate(text) if let == letter1]
+    let2_pos = [i for i, let in enumerate(text) if let == letter2]
+    if not let1_pos or not let2_pos:
+        return False
+    return all([i < j for i in let1_pos for j in let2_pos])
 
 
 def task6(text, censored):
     """
-    Given the censored text and the list of censored letters, output the uncensored text.
+    Given the censored text and the list of censored letters,
+    output the uncensored text.
 
     >>> task6('Wh*r* d*d my v*w*ls g*?', 'eeioeo')
     'Where did my vowels go?'
@@ -72,25 +78,28 @@ def task6(text, censored):
     'abcd'
     >>> task6('*PP*RC*S*', 'UEAE')
     'UPPERCASE'
-    """
-    # todo: write your code here
+    """ 
+    for c in censored:
+        text = text.replace('*', c, 1) 
+    return text
 
 
 def task7(text, words):
     """
-    Write a function that returns True if a given text can generate an array of words.
-        All strings are case insensitive.
+    Write a function that returns True if
+    a given text can generate an array of words.
+    All strings are case insensitive.
 
     >>> task7('Justin Bieber', ['injures', 'ebb', 'it'])
     True
     >>> task7('Natalie Portman', ['ornamental', 'pita'])
     True
     >>> task7('Chris Pratt', ['chirps', 'rat'])
-    True
+    False
     >>> task7('Jeff Goldblum', ['jog', 'meld', 'bluffs'])
     False
     """
-    # todo: write your code here
+    return sorted(text.replace(' ', '').lower()) == sorted(c for w in words for c in w)
 
 
 if __name__ == '__main__':
